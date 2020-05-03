c94e9cc9a6c032f94c30630ff8d3ea760a9b1dea
jirkakrepl <jiri.krepl@gmail.com>
2020-05-03 13:01:15 +0200

    feat: added test which does not print to reporter
    on client side, `describe` and `it` blocks are not
    printed

---
+++ imports/ui/components/hello/hello.test.js
@@ -0,0 +1,41 @@
+describe('some test', () => {
+  it('can do something', () => {
+    const x = 1 + 1;
+  })
+});
+
+describe('some test', () => {
+  it('can do something', () => {
+    const x = 1 + 1;
+  })
+});
+
+describe('some test', () => {
+  it('can do something', () => {
+    const x = 1 + 1;
+  })
+});
+
+describe('some test', () => {
+  it('can do something', () => {
+    const x = 1 + 1;
+  })
+});
+
+describe('some test', () => {
+  it('can do something', () => {
+    const x = 1 + 1;
+  })
+});
+
+describe('some test', () => {
+  it('can do something', () => {
+    const x = 1 + 1;
+  })
+});
+
+describe('some test', () => {
+  it('can do something', () => {
+    const x = 1 + 1;
+  })
+});
